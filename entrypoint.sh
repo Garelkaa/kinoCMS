3127361613ded53c6f365fa5f9ae7656acdf5edf
bbylfg mac <a.shcherbak337@gmail.com>
2024-05-31 22:24:47 +0300

    "Removed database_script.py from entrypoint.sh, added imports and models to fill_table.py, and created instances of various models with fake data."

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').count() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
 
 # Run your database initialization scripts
-python database_script.py
 python fill_table.py
 
 # Start the Django development server
--- fill_table.py
+++ fill_table.py
@@ -5,7 +5,11 @@ import django
 django.setup()
 
 from faker import Faker
-from users.models import CustomUser  # Замените `your_app` на имя вашего приложения и `YourModel` на имя вашей модели
+from users.models import CustomUser
+from cinema.models import Cinema, Movie, CinemaHall, MovieSession
+from other.models import News, Promotions, Pages, ContactsPage
+from django.utils import timezone
+
 
 
 def fill_table(rows=10):
@@ -23,6 +27,61 @@ def fill_table(rows=10):
             card=fake.credit_card_number()[:16],  # Случайный номер карты
             language=fake.random_element(elements=("u", "r")),  # Случайный выбор языка
             # Другие поля вашей модели
+        )
+        
+        News.objects.create(
+                title=fake.sentence(),
+                created=timezone.now(),
+                description=fake.paragraph(),
+                main_image=fake.random_element(elements=('static/image/batman.jpeg', 'static/image/jocker.jpeg', 'static/image/spider.jpeg'))  # замените на ваш путь к изображению
+            )
+        Promotions.objects.create(
+                title=fake.sentence(),
+                created=timezone.now(),
+                description=fake.paragraph(),
+                main_image=fake.random_element(elements=('static/image/batman.jpeg', 'static/image/jocker.jpeg', 'static/image/spider.jpeg'))  # замените на ваш путь к изображению
+            )
+        Pages.objects.create(
+                title=fake.sentence(),
+                description=fake.paragraph(),
+                main_image=fake.random_element(elements=('static/image/batman.jpeg', 'static/image/jocker.jpeg', 'static/image/spider.jpeg'))  # замените на ваш путь к изображению
+            )
+        ContactsPage.objects.create(
+                title=fake.sentence(),
+                address=fake.address(),
+                coords_x=fake.longitude(),
+                coords_y=fake.latitude(),
+                logo=fake.random_element(elements=('static/image/batman.jpeg', 'static/image/jocker.jpeg', 'static/image/spider.jpeg'))  # замените на ваш путь к изображению
+            )
+        Cinema.objects.create(
+                title=fake.sentence(),
+                created=timezone.now(),
+                description=fake.paragraph(),
+                main_image=fake.random_element(elements=('static/image/batman.jpeg', 'static/image/jocker.jpeg', 'static/image/spider.jpeg')),  # замените на ваш путь к изображению
+                top_image=fake.random_element(elements=('static/image/batman.jpeg', 'static/image/jocker.jpeg', 'static/image/spider.jpeg')),  # замените на ваш путь к изображению
+                url_trailer="https://example.com/trailer",
+                active=True
+            )
+        Movie.objects.create(
+                title=fake.sentence(),
+                description=fake.paragraph(),
+                main_image=fake.random_element(elements=('static/image/batman.jpeg', 'static/image/jocker.jpeg', 'static/image/spider.jpeg')),  # замените на ваш путь к изображению
+                url_trailer="https://example.com/trailer",
+                type=fake.random_element(elements=('3D', '2D', 'IMAX')),  # случайно выбираем тип фильма
+                active=True
+            )
+        CinemaHall.objects.create(
+                number=fake.random_int(min=1, max=10),  # случайно выбираем номер зала от 1 до 10
+                description=fake.paragraph(),
+                scheme_image="static/image/map.png",  # замените на ваш путь к изображению
+                top_image=fake.random_element(elements=('static/image/batman.jpeg', 'static/image/jocker.jpeg', 'static/image/spider.jpeg')),  # замените на ваш путь к изображению
+                cinema_id=1  # айдишник кинотеатра, в который относится этот зал
+            )
+        MovieSession.objects.create(
+            time=fake.time(),
+            price=fake.random_int(min=50, max=200),  # случайно выбираем цену от 50 до 200
+            date=timezone.now().date(),
+            movie_id=1,  # айдишник фильма                cinemahall_id=1  # айдишник зала
         )
 
 
